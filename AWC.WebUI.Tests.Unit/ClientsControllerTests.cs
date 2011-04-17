f7ce6516457926d8bb29e41dba8cffc9352a8ce1
Michael Cox <cox.michael@gmail.com>
2011-04-17 13:04:11 -0400

    Changed around the unit tests a bit.

--- AWC.WebUI.Tests.Unit/ClientsControllerTests.cs
+++ AWC.WebUI.Tests.Unit/ClientsControllerTests.cs
@@ -18,7 +18,7 @@ namespace AWC.WebUI.Tests.Unit
     public class ClientsControllerTests
     {
         [Test]
-        public void Create_Returns_New_ClientEditViewModel()
+        public void Should_Get_Blank_Form_To_Create_New_Client()
         {
             // Arrange
             var repository = new Mock<IRepository>();
@@ -33,25 +33,115 @@ namespace AWC.WebUI.Tests.Unit
         }
 
         [Test]
-        public void Edit_GivenValidID_Returns_ClientEditViewModel()
+        public void Should_Display_Client_Info_Given_Valid_ClientID()
         {
             // Arrange
-            int validId = 1;
             var logger = new Mock<ILogger>();
 
             var repository = new Mock<IRepository>();
-            Client client = new Client {ClientId = validId};
+            Client client = new Client {ClientId = 1};
+
+            repository.Setup(s=> s.Single(It.IsAny<Expression<Func<Client, bool>>>())).Returns(client);
 
             ClientsController clientsController = new ClientsController(repository.Object, logger.Object);
 
             // Act
-            ActionResult result = clientsController.Edit(validId);
+            ActionResult result = clientsController.Edit(1);
 
             // Assert
             Assert.IsInstanceOf(typeof(ViewResult), result);
             Assert.IsInstanceOf(typeof(ClientEditViewModel), ((ViewResult)result).Model);
-            Assert.AreEqual(validId, ((ClientEditViewModel)((ViewResult)result).Model).ClientId);
+            Assert.AreEqual(1, ((ClientEditViewModel)((ViewResult)result).Model).ClientId);
         }
 
+        [Test]
+        public void Should_Save_New_Client_And_Redirect_To_Edit_Screen_If_Validation_Passes()
+        {
+            // Arrange
+            var logger = new Mock<ILogger>();
+
+            var repository = new Mock<IRepository>();
+            ClientEditViewModel clientEditViewModel = new ClientEditViewModel
+                                                          {
+                                                              ClientId = 0,
+                                                              FirstName = "Michael",
+                                                              LastName = "Cox",
+                                                              AddressLine1 = "123 Fake St",
+                                                              City = "Auburn",
+                                                              StateCode = "MA",
+                                                              CountyCode = "PG",
+                                                              NumberOfAdults = 2,
+                                                              NumberOfChildren = 2
+                                                          };
+
+            ClientsController clientsController = new ClientsController(repository.Object, logger.Object);
+
+            // Act
+            RedirectToRouteResult result = clientsController.Create(clientEditViewModel) as RedirectToRouteResult;
+
+            // Assert
+            Assert.IsTrue(clientsController.ModelState.IsValid);
+            repository.Verify(v => v.Add(It.IsAny<Client>()), Times.Once(), "Should add new client.");
+            repository.Verify(v => v.CommitChanges(), Times.Once(), "Should save data to repository.");
+            Assert.AreEqual("Edit", result.RouteValues["action"]);
+        }
+
+        [Test]
+        public void Should_Edit_Client_And_Redirect_To_Edit_Screen_If_Validation_Passes()
+        {
+            // Arrange
+            var logger = new Mock<ILogger>();
+
+            var repository = new Mock<IRepository>();
+            ClientEditViewModel clientEditViewModel = new ClientEditViewModel()
+            {
+                ClientId = 1,
+                FirstName = "Michael",
+                LastName = "Cox",
+                AddressLine1 = "123 Fake St",
+                City = "Auburn",
+                StateCode = "MA",
+                CountyCode = "PG",
+                NumberOfAdults = 2,
+                NumberOfChildren = 2
+            };
+
+            Client client = new Client {ClientId = 1};
+
+            repository.Setup(s => s.Single(It.IsAny<Expression<Func<Client, bool>>>())).Returns(client);
+
+            ClientsController clientsController = new ClientsController(repository.Object, logger.Object);
+
+            // Act
+            RedirectToRouteResult result = clientsController.Create(clientEditViewModel) as RedirectToRouteResult;
+
+            // Assert
+            Assert.IsTrue(clientsController.ModelState.IsValid);
+            repository.Verify(v => v.CommitChanges(), Times.Once(), "Should save data to repository.");
+            Assert.AreEqual("Edit", result.RouteValues["action"]);
+
+        }
+
+        [Test]
+        public void Should_Return_Validation_Errors_If_Invalid_Client_Data_Submitted()
+        {
+            // Arrange
+            var logger = new Mock<ILogger>();
+            var repository = new Mock<IRepository>();
+
+            Client client = new Client { ClientId = 1 };
+            repository.Setup(s => s.Single(It.IsAny<Expression<Func<Client, bool>>>())).Returns(client);
+
+            ClientsController clientsController = new ClientsController(repository.Object, logger.Object);
+
+            clientsController.ModelState.AddModelError("", "Dummy error message.");
+
+            // Act
+            ViewResult result = clientsController.Edit(new ClientEditViewModel()) as ViewResult;
+
+            // Assert
+            Assert.IsFalse(clientsController.ModelState.IsValid);
+            repository.Verify(v => v.CommitChanges(), Times.Never(), "Should not update the database with validation errors.");
+        }
     }
 }
