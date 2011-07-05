90289892b2fc97be5ec31f2d9fa8b8fda15e24da
Michael Cox <cox.michael@gmail.com>
2011-07-05 17:08:43 -0400

    Added page for displaying the schedule in calendar format.

--- AWC.WebUI/Controllers/ScheduleController.cs
+++ AWC.WebUI/Controllers/ScheduleController.cs
@@ -26,29 +26,42 @@ namespace AWC.WebUI.Controllers
         public ActionResult Confirmed()
         {
             // Load clients/appointments with eager loading
-            var clients = from c in _repository.All<Client>().Include("ClientNotes")
-                          join a in _repository.All<Appointment>().Include("RequestedItems") on c equals a.Client
-                          where (a.AppointmentStatusId == (byte)Constants.AppointmentStatusId.Scheduled || a.AppointmentStatusId == (byte)Constants.AppointmentStatusId.Rescheduled)
-                          && a.ScheduledDateTime.HasValue
-                          orderby a.ScheduledDateTime descending 
-                          select new ScheduledClient
-                          {
-                              ClientId = c.ClientId,
-                              FirstName = c.FirstName,
-                              LastName = c.LastName,
-                              PrimaryPhoneNumber = c.PrimaryPhoneNumber,
-                              PhoneNumberTypeId = c.PrimaryPhoneTypeId,
-                              ScheduledDateTime = a.ScheduledDateTime.Value,
-                              TwoDayConfirmation = a.TwoDayConfirmation,
-                              TwoWeekConfirmation = a.TwoWeekConfirmation,
-                              SentLetterOrEmail = a.SentLetterOrEmail,
-                              RequestedItems = a.RequestedItems,
-                              ClientNotes = c.ClientNotes
-                          };
+            var clients = GetScheduledClients();
 
             var scheduledViewModel = new ScheduleViewModel {Clients = clients};
 
             return View(scheduledViewModel);
+        }
+
+        public ActionResult Calendar()
+        {
+            return View();
+        }
+
+        public JsonResult CalendarEvents(int? start, int? end, string callback)
+        {
+            DateTime t = DateTime.Today;
+
+            var startDate = start.HasValue ? ConvertFromUnixTimestamp(start.Value) : DateTime.Today;
+            var endDate = end.HasValue
+                              ? ConvertFromUnixTimestamp(end.Value)
+                              : new DateTime(t.Year, t.Month, 1, 11, 59, 59).AddMonths(1).AddDays(-1);
+
+            var clients = GetScheduledClients();
+
+            // Pull events object specific to jQuery calendar format
+            var events = from c in clients.Where(client => client.ScheduledDateTime >= startDate && client.ScheduledDateTime <= endDate).ToList()
+                         select new
+                                    {
+                                        id = c.ClientId,
+                                        title = c.FirstName + " " + c.LastName,
+                                        start = c.ScheduledDateTime.ToString("s")
+                                    };
+
+
+
+            return Json(events, JsonRequestBehavior.AllowGet);
+            //return string.Format("{0}({1});", callback, "");
         }
 
         public ActionResult WaitList()
@@ -92,5 +105,40 @@ namespace AWC.WebUI.Controllers
             return View();
         }
 
+        private IEnumerable<ScheduledClient> GetScheduledClients()
+        {
+            return from c in _repository.All<Client>().Include("ClientNotes")
+                   join a in _repository.All<Appointment>().Include("RequestedItems") on c equals a.Client
+                   where
+                       (a.AppointmentStatusId == (byte) Constants.AppointmentStatusId.Scheduled ||
+                        a.AppointmentStatusId == (byte) Constants.AppointmentStatusId.Rescheduled)
+                       && a.ScheduledDateTime.HasValue
+                   orderby a.ScheduledDateTime descending
+                   select new ScheduledClient
+                              {
+                                  ClientId = c.ClientId,
+                                  FirstName = c.FirstName,
+                                  LastName = c.LastName,
+                                  PrimaryPhoneNumber = c.PrimaryPhoneNumber,
+                                  PhoneNumberTypeId = c.PrimaryPhoneTypeId,
+                                  ScheduledDateTime = a.ScheduledDateTime.Value,
+                                  TwoDayConfirmation = a.TwoDayConfirmation,
+                                  TwoWeekConfirmation = a.TwoWeekConfirmation,
+                                  SentLetterOrEmail = a.SentLetterOrEmail,
+                                  RequestedItems = a.RequestedItems,
+                                  ClientNotes = c.ClientNotes
+                              };
+        }
+
+        private string FormatDateToISO8601(DateTime dateTime)
+        {
+            return dateTime.ToString("yyyy-MM-dd") + "T" + dateTime.ToString("hh:mm:ss") + "Z";
+        }
+
+        private DateTime ConvertFromUnixTimestamp(int timestamp)
+        {
+            var dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+            return dateTime.AddSeconds(timestamp);
+        }
     }
 }
