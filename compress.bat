4644c202e1a528901b72c11616981545d2330c44
Michael Cox <cox.michael@gmail.com>
2012-01-22 19:10:17 -0500

    Added AjaxMin bat file for compressing css and js

---
+++ compress.bat
@@ -0,0 +1,9 @@
+set PATH=%~dp0;"C:\Program Files (x86)\Microsoft\Microsoft Ajax Minifier\"
+
+ajaxmin -clobber AWC.WebUI\Public\js\script.js -o AWC.WebUI\Public\js\script.min.js
+
+ajaxmin -clobber AWC.WebUI\Public\js\libs\fullcalendar.js AWC.WebUI\Public\js\libs\gcal.js AWC.WebUI\Public\js\libs\jquery.maskedinput-1.3.js AWC.WebUI\Public\js\libs\jquery.validate.js AWC.WebUI\Public\js\libs\jquery.validate.unobtrusive.js -o AWC.WebUI\Public\js\plugins.min.js
+
+ajaxmin -clobber AWC.WebUI\Public\css\fullcalendar.css AWC.WebUI\Public\css\global.css -o AWC.WebUI\Public\css\all.min.css
+
+pause
